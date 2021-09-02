741bf9825f828f9fe0707e9ee47adeacff35b586
Miqueias Belarmino <miqueiascostabac@gmail.com>
2021-09-02 10:12:12 -0300

    ordering with asc and desc criteria

--- app/Http/Controllers/TrackerController.php
+++ app/Http/Controllers/TrackerController.php
@@ -28,19 +28,31 @@ class TrackerController extends Controller
 
             $location = $response->object()->results[0]->locations[0];
             if (!empty($location->adminArea1) || !empty($location->adminArea3) || !empty($location->adminArea5)) {
-                $hotels = (new HotelController)->getHotels($location->latLng->lat,$location->latLng->lng);
-                if($request->order =="price")
-                {
-                    usort($hotels, function($a, $b) {
-                        return $a['price'] <=> $b['price'];
-                    });
-                }else{
-                    usort($hotels, function($a, $b) {
-                        return $a['distance'] <=> $b['distance'];
-                    });
+                $hotels = (new HotelController)->getHotels($location->latLng->lat, $location->latLng->lng);
+                if ($request->order == "price") {
+                    if ($request->type == "asc") {
+                        usort($hotels, function ($a, $b) {
+                            return $a['price'] <=> $b['price'];
+                        });
+                    } else {
+                        usort($hotels, function ($a, $b) {
+                            return $b['price'] <=> $a['price'];
+                        });
+                    }
+                } else {
+                    if ($request->type == "asc") {
+                        usort($hotels, function ($a, $b) {
+                            return $a['distance'] <=> $b['distance'];
+                        });
+                    } else {
+                        usort($hotels, function ($a, $b) {
+                            return $b['distance'] <=> $a['distance'];
+                        });
+                    }
                 }
+
                 $hotels = array_slice($hotels, 0, 10);
-                return view('tracker.list',compact('hotels'));
+                return view('tracker.list', compact('hotels'));
             } else {
                 return redirect()->back()->with(['error' => 'Failed to track specified location!']);
             }
--- resources/views/tracker/search.blade.php
+++ resources/views/tracker/search.blade.php
@@ -13,17 +13,30 @@
                     <hr class="my-4">
                     <form action="{{ route('search.submit')}}" method="POST">
                         {{@csrf_field()}}
-                        <div class="">
-                            Order by:&nbsp;
-                            <input class="form-check-input" type="radio" name="order" id="proximity" value="proximity" checked>
-                            <label class="form-check-label" for="proximity">
-                                Proximity
-                            </label>
-                            <input class="form-check-input" type="radio" name="order" id="price" value="price">
-                            <label class="form-check-label" for="price">
-                                Price
-                            </label>
+                        <div class="row">
+                            <div style="width: 60%;">
+                                Order by:&nbsp;
+                                <input class="form-check-input" type="radio" name="order" id="proximity" value="proximity" checked>
+                                <label class="form-check-label" for="proximity">
+                                    Proximity
+                                </label>
+                                <input class="form-check-input" type="radio" name="order" id="price" value="price">
+                                <label class="form-check-label" for="price">
+                                    Price
+                                </label>
+                            </div>
+                            <div style="width: 40%;">
+                                <input class="form-check-input" type="radio" name="type" id="asc" value="asc" checked>
+                                <label class="form-check-label" for="asc">
+                                    A-Z
+                                </label>
+                                <input class="form-check-input" type="radio" name="type" id="desc" value="desc">
+                                <label class="form-check-label" for="desc">
+                                    Z-A
+                                </label>
+                            </div>
                         </div>
+
                         <!-- <label class=" mt-4" for="location">Location</label> -->
                         <div class="mb-3 mt-3">
                             <input type="text" class="form-control" name="location" id="location" placeholder="Location" required>
